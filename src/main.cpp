85fa8f74264452e4b23056fa08133e37165f1ac3
Abdullah <aabdullahmamin@gmail.com>
2024-09-19 22:15:27 +0400

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
-int main()
+#include "raytracer.hpp"
+
+int main(int argc, char **argv)
 {
+	if (argc != 5)
+	{
+		std::cout << "Usage: raytracer scene.lua out.bmp size max_bounces\n";
+		return 1;
+	}
+
+	Scene scene(atoi(argv[3]), argv[1]);
+	scene.print();
+	scene.render(argv[2]);
+
+	return 0;
 }

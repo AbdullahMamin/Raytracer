2d09662b3b66516edd6fb1bae41fb319faa32b9b
Abdullah <aabdullahmamin@gmail.com>
2024-09-19 22:14:29 +0400

    added vectors, rays and their associated functions and operators

---
+++ src/vector.cpp
@@ -0,0 +1,58 @@
+#include "vector.hpp"
+
+f32 Vector::magnitude() const
+{
+	return sqrtf(x*x + y*y + z*z);
+}
+
+Vector Vector::normalize() const
+{
+	f32 inv_magnitude = 1.f/magnitude();
+	return inv_magnitude*(*this);
+}
+
+f32 Vector::dot(Vector v) const
+{
+	return x*v.x + y*v.y + z*v.z;
+}
+
+Vector Vector::cross(Vector v) const
+{
+	return {y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x};
+}
+
+Vector Vector::reflect(Vector normal) const
+{
+	assert(fabs(normal.magnitude() - 1.f) < EPSILON); // make sure normal is unit length
+	return *this - 2.f*dot(normal)*normal;
+}
+
+Vector Ray::at(f32 t)
+{
+	return origin + t*direction;
+}
+
+Vector operator+(Vector v1, Vector v2)
+{
+	return {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+}
+
+Vector operator-(Vector v1, Vector v2)
+{
+	return {v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
+}
+
+Vector operator*(Vector v1, Vector v2)
+{
+	return {v1.x*v2.x, v1.y*v2.y, v1.z*v2.z};
+}
+
+Vector operator*(f32 s, Vector v)
+{
+	return {s*v.x, s*v.y, s*v.z};
+}
+
+Vector operator/(Vector v1, Vector v2)
+{
+	return {v1.x/v2.x, v1.y/v2.y, v1.z/v2.z};
+}
--- src/vector.hpp
+++ src/vector.hpp
@@ -1,6 +1,32 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 #include <cmath>
+#include <cassert>
 #include "types.hpp"
+
+#define EPSILON (1e-5f)
+
+struct Vector
+{
+	f32 x, y, z;
+
+	f32 magnitude() const;
+	Vector normalize() const;
+	f32 dot(Vector v) const;
+	Vector cross(Vector v) const;
+	Vector reflect(Vector normal) const;
+};
+
+struct Ray
+{
+	Vector origin, direction;
+	Vector at(f32 t);
+};
+
+Vector operator+(Vector v1, Vector v2);
+Vector operator-(Vector v1, Vector v2);
+Vector operator*(Vector v1, Vector v2);
+Vector operator*(f32 s, Vector v);
+Vector operator/(Vector v1, Vector v2);
 
 #endif // VECTOR_HPP

cb9e2981749686ab7c61bfdadc5be3030f06c014
Abdullah <aabdullahmamin@gmail.com>
2024-09-20 21:19:56 +0400

    Made so color adding doesn't care about alpha and just adds channels normally

--- src/color.cpp
+++ src/color.cpp
@@ -3,9 +3,9 @@
 Color operator+(Color c1, Color c2)
 {
 	Color result;
-	result.r = std::min(std::max((1.f - c2.a)*c1.r + c2.a*c2.r, 0.f), 1.f);
-	result.g = std::min(std::max((1.f - c2.a)*c1.g + c2.a*c2.g, 0.f), 1.f);
-	result.b = std::min(std::max((1.f - c2.a)*c1.b + c2.a*c2.b, 0.f), 1.f);
+	result.r = std::min(std::max(c1.r + c2.r, 0.f), 1.f);
+	result.g = std::min(std::max(c1.g + c2.g, 0.f), 1.f);
+	result.b = std::min(std::max(c1.b + c2.b, 0.f), 1.f);
 	return result;
 }
 
